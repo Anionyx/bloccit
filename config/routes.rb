a0bfe33525c22837667a1e9f899de15328d4bba1
anionyx <rey@innovativeops.co>
2014-07-11 01:50:08 +0000

    Implemented voting

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Bloccit::Application.routes.draw do
   resource :users, only: [:update]
 
   resources :topics do
-      resources :posts, except: [:index]
+      resources :posts, except: [:index] do 
       resources :comments, only: [:create, :destroy]
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
     end
+  end
+  
 
   get 'about' => 'welcome#about'
 
