935633a60063a522d1dc7a14635758a4c4f561d5
anionyx <rey@innovativeops.co>
2014-08-26 22:48:33 +0000

    fix finally mailer

--- app/mailers/favoritemailer.rb
+++ app/mailers/favoritemailer.rb
@@ -6,11 +6,11 @@ class Favoritemailer < ActionMailer::Base
     @post = post
     @comment = comment
 
-    headers["Message-ID"] = "<comments/#{@comment.id}@reybloccit.herokuapp.com>"
-    headers["In-Reply-To"] = "<post/#{@post.id}@reybloccit.herokuapp.com>"
-    headers["References"] = "<post/#{@post.id}@reybloccit.herokuapp.com>"
+    headers["Message-ID"] = "<comments/#{@comment.id}@reybloccit>"
+    headers["In-Reply-To"] = "<post/#{@post.id}@reybloccit>"
+    headers["References"] = "<post/#{@post.id}@reybloccit>"
 
-    mail(to: user.email, subject "New comment on #{post.title}")
+    mail(to: user.email, subject: "New comment on #{post.title}")
   end
 end
 
