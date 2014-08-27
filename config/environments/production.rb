618dfee058591906971f550cae6e99498f4fa221
anionyx <rey@innovativeops.co>
2014-08-27 14:52:20 +0000

    last compile fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Bloccit::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = [*.js','*.css','*.css.erb]
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
